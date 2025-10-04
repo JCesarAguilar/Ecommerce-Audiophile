464727823793567784ffc4cf49a2709da6e82385
JCesarAguilar <jcesar.a.15@gmail.com>
2025-10-03 23:07:40 -0500

    commit proyecto completo

--- src/interfaces/ICategory.ts
+++
@@ -1,6 +0,0 @@
-export interface ICategory {
-  image: string;
-  title: string;
-  cta: string;
-  route: string;
-}
--- src/interfaces/IOrder.ts
+++
@@ -1,8 +0,0 @@
-import { IProduct } from "./IProduct";
-
-export interface IOrder {
-  id: number;
-  products: IProduct[];
-  date: string;
-  status: string;
-}
